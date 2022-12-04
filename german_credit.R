9d10dd0aaaa847124f8cf5d2d24cbfa544a60a64
GaussTheBoss <semerhi@gmail.com>
2022-12-04 17:31:51 -0500

    formatting

--- german_credit.R
+++ german_credit.R
@@ -3,17 +3,17 @@ library(tidymodels)
 library(readr)
 
 init <- function() {
-    # 
+    #
     # Function to load saved model into global variable: model
-    # 
+    #
     load("trained_model.RData")
     model <<- logreg_fit
 }
 
 score <- function(data) {
-    # 
+    #
     # Function to predict on input data (data.frame)
-    # 
+    #
     df <- data.frame(data, stringsAsFactors = F)
     preds <- predict(model, df)
     output <- list(label_value = df$label, score = preds$.pred_class)
@@ -21,11 +21,16 @@ score <- function(data) {
 }
 
 metrics <- function(data) {
-    # 
-    # Function to compute binary classification metrics on scored and labeled data
-    # 
+    #
+    # Function to compute binary classification metrics on scored and labeled 
+    # data
+    #
     df <- data.frame(data)
-    get_metrics <- metric_set(f_meas, accuracy, sensitivity, specificity, precision)
-    output <- get_metrics(data = df, truth = as.factor(label_value), estimate = as.factor(score))
+    get_metrics <- metric_set(
+        f_meas, accuracy, sensitivity, specificity, precision
+    )
+    output <- get_metrics(
+        data = df, truth = as.factor(label_value), estimate = as.factor(score)
+    )
     return(output)
 }
